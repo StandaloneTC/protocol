d55e3447a7d4158d44d7e5c5d8571d7f61bc11eb
berberman <1793913507@qq.com>
2018-11-17 23:37:51 +0800

    [Gradle] add bintray release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.novoda.gradle.release.PublishExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
@@ -8,12 +9,17 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.dokka:dokka-gradle-plugin")
+        classpath("com.novoda:bintray-release:+")
     }
 }
 
 plugins {
     kotlin("jvm") version "1.3.10"
     id("org.jetbrains.dokka") version "0.9.17"
+}
+
+apply {
+    plugin("com.novoda.bintray-release")
 }
 
 group = "tech.standalonetc"
@@ -32,4 +38,13 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+configure<PublishExtension> {
+    userOrg = "standalonetc"
+    groupId = "tech.standalonetc"
+    artifactId = "protocol"
+    publishVersion = version.toString()
+    desc = "communication protocol for StandaloneTC."
+    website = "https://github.com/StandaloneTC/protocol"
 }
